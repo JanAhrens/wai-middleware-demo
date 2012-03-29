6096f32a6d6cb336fcc69b3ba40b993cf1b2a11e
Jan Ahrens <jan.ahrens+git@googlemail.com>
2012-03-28 23:34:23 -0700

    removed some ghc warnings

--- CancelRequestOnMissingParam.hs
+++ CancelRequestOnMissingParam.hs
@@ -9,38 +9,42 @@ import qualified Data.ByteString.Lazy as LBS
 import Control.Monad.IO.Class (liftIO)
 import Network.Wai (Request(..), Response(..), responseLBS)
 import Network.Wai.Handler.Warp (run)
-import Network.Wai.Parse (getRequestBodyType, sinkRequestBody, lbsBackEnd, fileName, Param, File)
+import Network.Wai.Parse ( getRequestBodyType, sinkRequestBody
+                         , lbsBackEnd, fileName, Param, File)
 import Network.HTTP.Types (statusOK, statusPreconditionFailed)
 
+main :: IO ()
 main = run 3000 (requireParameter ("version", "1") application)
-
-application _ = return $
-  responseLBS statusOK [("Content-Type", "text/plain")] "Hello World"
+  where
+    application _ =return $
+      responseLBS statusOK [("Content-Type", "text/plain")] "Hello World"
 
 requireParameter requiredParameter app env = do
-  body       <- extractBody env
+  body       <- extractBody
   postParams <- extractPostParams env body
   getParams  <- extractGetParams  env
 
   case parameterPresent requiredParameter (getParams ++ postParams) of
     Just _  -> app env { requestBody = CL.sourceList body }
     Nothing -> return $
-      responseLBS statusPreconditionFailed [("Content-Type", "application/json")] "{\"code\": \"PARAMETER_MISSING\"}"
+      responseLBS statusPreconditionFailed
+        [("Content-Type", "application/json")]
+        "{\"code\": \"PARAMETER_MISSING\"}"
   where
     parameterPresent (paramName, paramValue) params = do
       result <- lookup paramName params
       if result == paramValue
         then Just result
         else Nothing
-    extractBody env = requestBody env C.$$ CL.consume
+    extractBody = requestBody env C.$$ CL.consume
 
 extractPostParams env body =
   if requestMethod env `elem` ["GET", "HEAD"]
     then return []
-    else do postParams <- liftIO $ allPostParams body
+    else do postParams <- liftIO allPostParams
             return $ collectPostParams postParams
   where
-    allPostParams body = case getRequestBodyType env of
+    allPostParams = case getRequestBodyType env of
       Nothing  -> return ([], [])
       Just rbt -> C.runResourceT $ CL.sourceList body C.$$ sinkRequestBody lbsBackEnd rbt
     collectPostParams (postParams, files) = postParams ++
